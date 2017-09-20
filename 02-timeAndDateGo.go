68b68f20a31abfc119cc031b33d4628845b630c6
GARY CONNELLY - Student <G00336837@gmit.ie>
2017-09-20 10:41:33 +0100

    Problem 2 complete

---
+++ 02-timeAndDateGo.go
@@ -0,0 +1,21 @@
+//Author: Gary Connelly
+//date 20/09/2017
+
+
+//Adapted from https://tour.golang.org/welcome/4
+
+
+
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+}
