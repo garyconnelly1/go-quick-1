57501e64e2c09687266424ad879b23af9f82b15d
ZqhKHP <G00336837@gmit.ie>
2017-09-22 13:44:07 +0100

    create forr loop printing 1-100

--- 03-fizzbuzz.go
+++ 03-fizzbuzz.go
@@ -1,2 +1,21 @@
 //Author: Gary Connelly
-//date 20/09/2017
+//date 22/09/2017
+
+
+package main
+
+import (
+	"fmt"
+	
+)
+//main methof
+
+func main() {
+	sum:= 0;
+	//basic for loop to print from 1-100
+	for i := 0; i < 100; i++ {
+    sum += i
+	fmt.Println(sum)
+  }
+  
+}
