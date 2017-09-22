1f59ecd6dfdb6deb33c2d19b8eca69209bcc9f04
ZqhKHP <G00336837@gmit.ie>
2017-09-22 15:12:29 +0100

    function for calculating factorial 100

--- 04-factorialDigitSum.go
+++ 04-factorialDigitSum.go
@@ -1,2 +1,22 @@
 //Author: Gary Connelly
 //date 22/09/2017
+
+//adapted from https://gist.github.com/shockalotti/7fa310e915ee66766039
+
+package main
+
+import "fmt"
+
+func factorial(x int) int {
+	if x == 0 {
+		return 1
+	}
+	
+	return x * factorial(x-1)
+}
+
+func main() {
+	x := int(100)
+	calcFactorial := factorial(x)
+	fmt.Println(calcFactorial)
+}
