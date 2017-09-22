7f0c59402be2e57c856542edbc8f676c79e0a963
ZqhKHP <G00336837@gmit.ie>
2017-09-22 15:47:49 +0100

    creating the scan for the user input

--- 05-guessingGame.go
+++ 05-guessingGame.go
@@ -1,2 +1,14 @@
 //Author: Gary Connelly
 //date 22/09/2017
+
+package main
+
+import "fmt"
+
+const myNumber = 5
+
+func main() {
+	var guess int
+	fmt.Println("Guess my number: ")
+	fmt.Scanf("%v", &guess)
+}
