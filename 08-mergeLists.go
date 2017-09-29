8a3d093fde3a4bce4a0b848af4559d2bb583d84f
ZqhKHP <G00336837@gmit.ie>
2017-09-29 22:43:35 +0100

    commenting code and fix errors(still one error)

--- 08-mergeLists.go
+++ 08-mergeLists.go
@@ -13,6 +13,7 @@ func main () {
 	 x := [] int {2,4,6}
 	y := [] int {1,5,7}
   
+  //displaying the lists
 	fmt.Println("List 1 is: ")
 	for _, v := range x {
     fmt.Println(v)
@@ -24,9 +25,10 @@ func main () {
 	}
   
 	//merge lists
-	var list3 [] int = merge (x, y)
+	var list3 [] int = merge (x, y) //merge is the function from below
 	fmt.Println("The merged list is: ")
   
+  //iterate through the merged sorted list
 	for _, v := range list3 {
     fmt.Println(v)
 	}//end for
@@ -39,17 +41,19 @@ func main () {
 func merge (list1 [] int, list2 [] int) [] int{
   var list3 [] int = len(list1) + len(list2)
   
+  //create the first half of list3 using list 1
   for i := 0; i < len(list1); i++ {
     list3[i] = list1[i]
     
   }//end for
   
+  //create the second half of list3 using list 2
   for i = 0 , j := len(list1); i < len(list2); i++ , j++ { 
     list3[j] = list2[i]
     
   }//end for
   
-   sort(list3)
+   sort(list3)//call function that sorts the list
    return list3
   
 }//end merge method
@@ -61,10 +65,11 @@ func sort (list [] int){
      var minIndex int = i
      
     for j := i + 1; j < len(list); j++{
-      
+      //bringing the smallest element of the list to the front of the index
       if (list[j] < min){
         
         min = list[j]
+		//change the index
         minIndex = j
         
       }//end if
