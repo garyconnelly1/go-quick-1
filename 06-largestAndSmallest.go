c5c65c2030ab65960f257bd0daca2af1c7818af1
ZqhKHP <G00336837@gmit.ie>
2017-09-29 16:49:13 +0100

    comments to explain my code

--- 06-largestAndSmallest.go
+++ 06-largestAndSmallest.go
@@ -7,6 +7,7 @@ package main
 
 import "fmt"
 
+//main method
 func main() {
 
 //create list of random numbers
@@ -14,8 +15,11 @@ func main() {
 	 26, 42, 17, 10, 4, 67, 98, 31, 82, 56,
 	}
 	
+	//initialise the smallest and biggest to the first element in the list
 	smallest, biggest := x[0], x[0]
-	for _, v := range x {
+	//for loop to iterate through list x
+	for _, v := range x { // * Range - method in go for iterating through the contents of an array
+		//if statements to compare the size of the element in the list
 		if v > biggest {
 			biggest = v
 		}
@@ -24,6 +28,7 @@ func main() {
 		}
 	}
 	
+	//outputs the smallest and biggest numbers
 	fmt.Println("The biggest number is ", biggest)
 	fmt.Println("The smallest number is ", smallest)
 }
