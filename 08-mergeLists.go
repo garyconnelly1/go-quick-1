5dcf8c18d1dc45f15a4611980aec110d1f7f223d
ZqhKHP <G00336837@gmit.ie>
2017-09-29 22:29:59 +0100

    creating the main method

--- 08-mergeLists.go
+++ 08-mergeLists.go
@@ -10,4 +10,26 @@ import
 
 
 func main () {
-}
+	 x := [] int {2,4,6}
+	y := [] int {1,5,7}
+  
+	fmt.Println("List 1 is: ")
+	for _, v := range x {
+    fmt.Println(v)
+	}
+  
+	fmt.Println("List 2 is: ")
+	for _, v := range y {
+    fmt.Println(v)
+	}
+  
+	//merge lists
+	var list3 [] int = merge (x, y)
+	fmt.Println("The merged list is: ")
+  
+	for _, v := range list3 {
+    fmt.Println(v)
+	}//end for
+	fmt.Println()
+
+}//end main
