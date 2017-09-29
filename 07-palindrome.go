ed225c96e0bb08d64dafca6282807943cd0f0345
ZqhKHP <G00336837@gmit.ie>
2017-09-29 17:39:05 +0100

    adding comments to make the code readable

--- 07-palindrome.go
+++ 07-palindrome.go
@@ -8,17 +8,23 @@ import (
 	"strings"
 )
 
+//main method
 func main() {
  	var word string
 	fmt.Println("Enter string:")
+	//scanf to take in user input
 	fmt.Scanf("%s\n", &word)
+	convert all letters to lower case
 	word = strings.ToLower(word)
+	//call the palindromeChecker method
 	fmt.Println(palindromeChecker(word))
 }
 
+//method to check if the string is a palindrome
 func palindromeChecker(s string) string {
 	middle := len(s) / 2
 	last := len(s) - 1
+	//for each letter up to the middle of the word, check if its the same as the corresponding letter from the end of the string
 	for i := 0; i < middle; i++ {
 		if s[i] != s[last-i] {
 			return "NO. It's not a Palimdrome."
