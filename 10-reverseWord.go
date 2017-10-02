6e3090f145cc70a75200750b14bdb13831bb1d2f
ZqhKHP <G00336837@gmit.ie>
2017-10-02 19:00:35 +0100

    commenting code

--- 10-reverseWord.go
+++ 10-reverseWord.go
@@ -16,9 +16,11 @@ func main() {
 	fmt.Printf("The word '%s' is %s" spelt backwards!, word, reverseWord(word)) // Print string in reverse
 }
 
+//reverse function
 func reverseWord(s string) string {
 	var reverse string
 	
+	//for i starting at the end of the word and parsing each letter into the variable reverse
     for i := len(s)-1; i >= 0; i-- {
         reverse += string(s[i])
     }
