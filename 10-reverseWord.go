fc3a1c4ed2fdd96c326af627613b1ebaebec6ba3
ZqhKHP <G00336837@gmit.ie>
2017-10-02 17:03:55 +0100

    main method

--- 10-reverseWord.go
+++ 10-reverseWord.go
@@ -1,2 +1,15 @@
 //Author: Gary Connelly
 //date 02/10/2017
+
+package main
+
+import (
+	"fmt" 
+)
+
+
+func main() {
+	word := "HelloWorld" // Word to reverse
+
+	fmt.Printf("The reverse of the String '%s' is %s", word, reverseWord(word)) // Print string in reverse
+}
