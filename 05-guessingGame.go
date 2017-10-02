834936565d0575e0debdb9f439055b19dd009692
ZqhKHP <G00336837@gmit.ie>
2017-10-02 20:04:43 +0100

    finishing guessing game

--- 05-guessingGame.go
+++ 05-guessingGame.go
@@ -25,3 +25,38 @@ func main (){
 	numFound := false
 	
 	//for loop for number Guessing
+	for numFound != true {
+	  
+	  
+	  fmt.Println("Guess my number:(guess must be between 1 and 100) ")
+	  fmt.Scanf("%v", &guess)
+	  
+	  
+	  if guess == previousGuess {
+	    fmt.Println("That was the same as your Last guess! Try again")
+	    count --
+	  }//end first if
+	  
+	  if guess > myNum {
+	    fmt.Println("To big! Please try again...")
+	    count ++
+	  }//end if
+	  
+	  if guess < myNum {
+	     fmt.Println("To small! Please try again...")
+	    count ++
+	    
+	  }//end if
+	  
+	  if guess == myNum {
+	    numFound = true
+	  }//end if
+	  
+	  previousGuess = guess
+	}//end for loop 
+	
+ fmt.Println("Correct! My number was : %v ", myNum)
+ fmt.Println("And it only took you %v tries!", count)
+ 
+ 
+}//end main
