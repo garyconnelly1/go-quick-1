7648b53f630f0733f363cba9b3c5ea7cef6bc644
ZqhKHP <G00336837@gmit.ie>
2017-10-02 20:05:53 +0100

    change date

--- 05-guessingGame.go
+++ 05-guessingGame.go
@@ -1,5 +1,5 @@
 //Author: Gary Connelly
-//date 22/09/2017
+//date 02/10/2017
 
 //Write a guessing game where the user must guess a randomly generated number. After every guess the program tells the user whether their number was too high or too low. At the end, the number of tries should be printed. It counts only as one try if they input the same number multiple times consecutively.
 
